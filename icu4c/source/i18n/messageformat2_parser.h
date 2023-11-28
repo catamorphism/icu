c99ccbafa5595e9cd4bbb2f21acad4e05c6f8f41
Tim Chevalier <tjc@igalia.com>
2023-11-27 20:16:25 -0800

    Re-indent Parser class

--- icu4c/source/i18n/messageformat2_parser.h
+++ icu4c/source/i18n/messageformat2_parser.h
@@ -6,119 +6,123 @@
 #ifndef MESSAGEFORMAT_PARSER_H
 #define MESSAGEFORMAT_PARSER_H
 
+#include "unicode/messageformat2.h"
+
 #if U_SHOW_CPLUSPLUS_API
 
 #if !UCONFIG_NO_FORMATTING
 
-U_NAMESPACE_BEGIN  namespace message2 {
+U_NAMESPACE_BEGIN
 
-      using namespace data_model;
+namespace message2 {
 
-      // Parser class (private)
-      class Parser : public UMemory {
-      public:
-          virtual ~Parser();
-          static Parser* create(const UnicodeString &input, MessageFormatDataModel::Builder& dataModelBuilder, UnicodeString& normalizedInput, Errors& errors, UErrorCode& errorCode) {
-              if (U_FAILURE(errorCode)) {
-                  return nullptr;
-              }
-              Parser* p = new Parser(input, dataModelBuilder, errors, normalizedInput);
-              if (p == nullptr) {
-                  errorCode = U_MEMORY_ALLOCATION_ERROR;
-              }
-              return p;
-          }
-          // The parser validates the message and builds the data model
-          // from it.
-          void parse(UParseError &, UErrorCode &);
-      private:
-          friend class MessageFormatDataModel::Builder;
+    using namespace data_model;
 
-          /*
-            Use an internal "parse error" structure to make it easier to translate
-            absolute offsets to line offsets.
-            This is translated back to a `UParseError` at the end of parsing.
-          */
-          typedef struct MessageParseError {
-              // The line on which the error occurred
-              uint32_t line;
-              // The offset, relative to the erroneous line, on which the error occurred
-              uint32_t offset;
-              // The total number of characters seen before advancing to the current line. It has a value of 0 if line == 0.
-              // It includes newline characters, because the index does too.
-              uint32_t lengthBeforeCurrentLine;
+    // Parser class (private)
+    class Parser : public UMemory {
+    public:
+	virtual ~Parser();
+	static Parser* create(const UnicodeString &input, MessageFormatDataModel::Builder& dataModelBuilder, UnicodeString& normalizedInput, StaticErrors& errors, UErrorCode& errorCode) {
+	  if (U_FAILURE(errorCode)) {
+	    return nullptr;
+	  }
+	  Parser* p = new Parser(input, dataModelBuilder, errors, normalizedInput);
+	  if (p == nullptr) {
+	    errorCode = U_MEMORY_ALLOCATION_ERROR;
+	  }
+	  return p;
+	}
+	// The parser validates the message and builds the data model
+	// from it.
+	void parse(UParseError &, UErrorCode &);
+    private:
+	friend class MessageFormatDataModel::Builder;
 
-              // This parser doesn't yet use the last two fields.
-              UChar   preContext[U_PARSE_CONTEXT_LEN];
-              UChar   postContext[U_PARSE_CONTEXT_LEN];
-          } MessageParseError;
+	/*
+	  Use an internal "parse error" structure to make it easier to translate
+	  absolute offsets to line offsets.
+	  This is translated back to a `UParseError` at the end of parsing.
+	*/
+	typedef struct MessageParseError {
+	    // The line on which the error occurred
+	    uint32_t line;
+	    // The offset, relative to the erroneous line, on which the error occurred
+	    uint32_t offset;
+	    // The total number of characters seen before advancing to the current line. It has a value of 0 if line == 0.
+	    // It includes newline characters, because the index does too.
+	    uint32_t lengthBeforeCurrentLine;
 
-          Parser(const UnicodeString &input, MessageFormatDataModel::Builder& dataModelBuilder, Errors& e, UnicodeString& normalizedInputRef)
-              : source(input), index(0), errors(e), normalizedInput(normalizedInputRef), dataModel(dataModelBuilder) {
-              parseError.line = 0;
-              parseError.offset = 0;
-              parseError.lengthBeforeCurrentLine = 0;
-              parseError.preContext[0] = '\0';
-              parseError.postContext[0] = '\0';
-          }
+	    // This parser doesn't yet use the last two fields.
+	    UChar   preContext[U_PARSE_CONTEXT_LEN];
+	    UChar   postContext[U_PARSE_CONTEXT_LEN];
+	} MessageParseError;
 
-          // Used so `parseEscapeSequence()` can handle all types of escape sequences
-          // (literal, text, and reserved)
-          typedef enum { LITERAL, TEXT, RESERVED } EscapeKind;
+	Parser(const UnicodeString &input, MessageFormatDataModel::Builder& dataModelBuilder, StaticErrors& e, UnicodeString& normalizedInputRef)
+	  : source(input), index(0), errors(e), normalizedInput(normalizedInputRef), dataModel(dataModelBuilder) {
+	  parseError.line = 0;
+	  parseError.offset = 0;
+	  parseError.lengthBeforeCurrentLine = 0;
+	  parseError.preContext[0] = '\0';
+	  parseError.postContext[0] = '\0';
+	}
 
-          static void translateParseError(const MessageParseError&, UParseError&);
-          static void setParseError(MessageParseError&, uint32_t);
-          void maybeAdvanceLine();
-          void parseBody(UErrorCode &);
-          void parseDeclarations(UErrorCode &);
-          void parseSelectors(UErrorCode &);
+	// Used so `parseEscapeSequence()` can handle all types of escape sequences
+	// (literal, text, and reserved)
+	typedef enum { LITERAL, TEXT, RESERVED } EscapeKind;
 
-          void parseWhitespaceMaybeRequired(bool, UErrorCode &);
-          void parseRequiredWhitespace(UErrorCode &);
-          void parseOptionalWhitespace(UErrorCode &);
-          void parseToken(UChar32, UErrorCode &);
-          void parseTokenWithWhitespace(UChar32, UErrorCode &);
-          template <int32_t N>
-          void parseToken(const UChar32 (&)[N], UErrorCode &);
-          template <int32_t N>
-          void parseTokenWithWhitespace(const UChar32 (&)[N], UErrorCode &);
-          UnicodeString parseName(UErrorCode&);
-          VariableName parseVariableName(UErrorCode&);
-          FunctionName parseFunction(UErrorCode&);
-          void parseEscapeSequence(EscapeKind, UnicodeString&, UErrorCode &);
-          void parseLiteralEscape(UnicodeString&, UErrorCode &);
-          Literal parseLiteral(UErrorCode &);
-          void parseOption(Operator::Builder&, UErrorCode&);
-          void parseOptions(Operator::Builder&, UErrorCode&);
-          void parseReservedEscape(UnicodeString&, UErrorCode&);
-          void parseReservedChunk(Reserved::Builder&, UErrorCode&);
-          Reserved parseReserved(UErrorCode &);
-          Operator parseAnnotation(UErrorCode &);
-          void parseLiteralOrVariableWithAnnotation(bool, Expression::Builder&, UErrorCode &);
-          Expression parseExpression(bool&, UErrorCode &);
-          void parseTextEscape(UnicodeString&, UErrorCode&);
-          UnicodeString parseText(UErrorCode&);
-          Key parseKey(UErrorCode&);
-          SelectorKeys parseNonEmptyKeys(UErrorCode&);
-          void errorPattern(UErrorCode&);
-          Pattern parsePattern(UErrorCode&);
+	static void translateParseError(const MessageParseError&, UParseError&);
+	static void setParseError(MessageParseError&, uint32_t);
+	void maybeAdvanceLine();
+	void parseBody(UErrorCode &);
+	void parseDeclarations(UErrorCode &);
+	void parseSelectors(UErrorCode &);
 
-          // The input string
-          const UnicodeString &source;
-          // The current position within the input string
-          uint32_t index;
-          // Represents the current line (and when an error is indicated),
-          // character offset within the line of the parse error
-          MessageParseError parseError;
+	void parseWhitespaceMaybeRequired(bool, UErrorCode &);
+	void parseRequiredWhitespace(UErrorCode &);
+	void parseOptionalWhitespace(UErrorCode &);
+	void parseToken(UChar32, UErrorCode &);
+	void parseTokenWithWhitespace(UChar32, UErrorCode &);
+	template <int32_t N>
+	void parseToken(const UChar32 (&)[N], UErrorCode &);
+	template <int32_t N>
+	void parseTokenWithWhitespace(const UChar32 (&)[N], UErrorCode &);
+	UnicodeString parseName(UErrorCode&);
+	VariableName parseVariableName(UErrorCode&);
+	FunctionName parseFunction(UErrorCode&);
+	void parseEscapeSequence(EscapeKind, UnicodeString&, UErrorCode &);
+	void parseLiteralEscape(UnicodeString&, UErrorCode &);
+	Literal parseLiteral(UErrorCode &);
+	void parseOption(Operator::Builder&, UErrorCode&);
+	void parseOptions(Operator::Builder&, UErrorCode&);
+	void parseReservedEscape(UnicodeString&, UErrorCode&);
+	void parseReservedChunk(Reserved::Builder&, UErrorCode&);
+	Reserved parseReserved(UErrorCode &);
+	Operator parseAnnotation(UErrorCode &);
+	void parseLiteralOrVariableWithAnnotation(bool, Expression::Builder&, UErrorCode &);
+	Expression parseExpression(bool&, UErrorCode &);
+	void parseTextEscape(UnicodeString&, UErrorCode&);
+	UnicodeString parseText(UErrorCode&);
+	Key parseKey(UErrorCode&);
+	SelectorKeys parseNonEmptyKeys(UErrorCode&);
+	void errorPattern(UErrorCode&);
+	Pattern parsePattern(UErrorCode&);
 
-          // The structure to use for recording errors
-          Errors& errors;
+	// The input string
+	const UnicodeString &source;
+	// The current position within the input string
+	uint32_t index;
+	// Represents the current line (and when an error is indicated),
+	// character offset within the line of the parse error
+	MessageParseError parseError;
 
-          // Normalized version of the input string (optional whitespace removed)
-          UnicodeString& normalizedInput;
+	// The structure to use for recording errors
+	StaticErrors& errors;
 
-          // The parent builder
-          MessageFormatDataModel::Builder &dataModel;
+	// Normalized version of the input string (optional whitespace removed)
+	UnicodeString& normalizedInput;
+
+	// The parent builder
+	MessageFormatDataModel::Builder &dataModel;
     }; // class Parser
 
 } // namespace message2
@@ -132,4 +136,3 @@ U_NAMESPACE_END
 
 #endif // U_HIDE_DEPRECATED_API
 // eof
-
