f37650660eebe39d1832155766da4f26ecbaf644
Tim Chevalier <tjc@igalia.com>
2023-12-04 18:00:24 -0800

    Indent messageformat2_context.h

--- icu4c/source/i18n/messageformat2_context.h
+++ icu4c/source/i18n/messageformat2_context.h
@@ -25,158 +25,158 @@ U_NAMESPACE_BEGIN
 
 namespace message2 {
 
-class Formatter;
-class FormatterFactory;
-class SelectorFactory;
+    class Formatter;
+    class FormatterFactory;
+    class SelectorFactory;
 
-extern void formatDateWithDefaults(const Locale& locale, UDate date, UnicodeString&, UErrorCode& errorCode);
-extern number::FormattedNumber formatNumberWithDefaults(const Locale& locale, double toFormat, UErrorCode& errorCode);
-extern number::FormattedNumber formatNumberWithDefaults(const Locale& locale, int32_t toFormat, UErrorCode& errorCode);
-extern number::FormattedNumber formatNumberWithDefaults(const Locale& locale, int64_t toFormat, UErrorCode& errorCode);
-extern number::FormattedNumber formatNumberWithDefaults(const Locale& locale, StringPiece toFormat, UErrorCode& errorCode);
-extern DateFormat* defaultDateTimeInstance(const Locale&, UErrorCode&);
+    extern void formatDateWithDefaults(const Locale& locale, UDate date, UnicodeString&, UErrorCode& errorCode);
+    extern number::FormattedNumber formatNumberWithDefaults(const Locale& locale, double toFormat, UErrorCode& errorCode);
+    extern number::FormattedNumber formatNumberWithDefaults(const Locale& locale, int32_t toFormat, UErrorCode& errorCode);
+    extern number::FormattedNumber formatNumberWithDefaults(const Locale& locale, int64_t toFormat, UErrorCode& errorCode);
+    extern number::FormattedNumber formatNumberWithDefaults(const Locale& locale, StringPiece toFormat, UErrorCode& errorCode);
+    extern DateFormat* defaultDateTimeInstance(const Locale&, UErrorCode&);
 
-using namespace data_model;
+    using namespace data_model;
 
-// Intermediate classes used internally in the formatter
+    // Intermediate classes used internally in the formatter
 
-// Closures and environments
-// -------------------------
+    // Closures and environments
+    // -------------------------
 
-class Environment;
+    class Environment;
 
-// A closure represents the right-hand side of a variable
-// declaration, along with an environment giving values
-// to its free variables
-class Closure : public UMemory {
-public:
-    const Expression& getExpr() const {
-        return expr;
-    }
-    const Environment& getEnv() const {
-        return env;
-    }
-    Closure(const Expression& expression, const Environment& environment) : expr(expression), env(environment) {}
-    Closure(Closure&&) = default;
+    // A closure represents the right-hand side of a variable
+    // declaration, along with an environment giving values
+    // to its free variables
+    class Closure : public UMemory {
+    public:
+        const Expression& getExpr() const {
+            return expr;
+        }
+        const Environment& getEnv() const {
+            return env;
+        }
+        Closure(const Expression& expression, const Environment& environment) : expr(expression), env(environment) {}
+        Closure(Closure&&) = default;
 
-    virtual ~Closure();
-private:
+        virtual ~Closure();
+    private:
 
-    // An unevaluated expression
-    const Expression& expr;
-    // The environment mapping names used in this
-    // expression to other expressions
-    const Environment& env;
-};
+        // An unevaluated expression
+        const Expression& expr;
+        // The environment mapping names used in this
+        // expression to other expressions
+        const Environment& env;
+    };
 
-// An environment is represented as a linked chain of
-// non-empty environments, terminating at an empty environment.
-// It's searched using linear search.
-class Environment : public UMemory {
-public:
-    virtual bool has(const VariableName&) const = 0;
-    virtual const Closure& lookup(const VariableName&) const = 0;
-    static Environment* create(UErrorCode&);
-    static Environment* create(const VariableName&, Closure&&, Environment*, UErrorCode&);
-    virtual ~Environment();
-};
+    // An environment is represented as a linked chain of
+    // non-empty environments, terminating at an empty environment.
+    // It's searched using linear search.
+    class Environment : public UMemory {
+    public:
+        virtual bool has(const VariableName&) const = 0;
+        virtual const Closure& lookup(const VariableName&) const = 0;
+        static Environment* create(UErrorCode&);
+        static Environment* create(const VariableName&, Closure&&, Environment*, UErrorCode&);
+        virtual ~Environment();
+    };
 
-class NonEmptyEnvironment;
-class EmptyEnvironment : public Environment {
-public:
-    EmptyEnvironment() = default;
-    virtual ~EmptyEnvironment();
+    class NonEmptyEnvironment;
+    class EmptyEnvironment : public Environment {
+    public:
+        EmptyEnvironment() = default;
+        virtual ~EmptyEnvironment();
 
-private:
-    friend class Environment;
+    private:
+        friend class Environment;
 
-    bool has(const VariableName&) const override;
-    const Closure& lookup(const VariableName&) const override;
-    static EmptyEnvironment* create(UErrorCode&);
-    static NonEmptyEnvironment* create(const VariableName&, Closure&&, Environment*, UErrorCode&);
-};
+        bool has(const VariableName&) const override;
+        const Closure& lookup(const VariableName&) const override;
+        static EmptyEnvironment* create(UErrorCode&);
+        static NonEmptyEnvironment* create(const VariableName&, Closure&&, Environment*, UErrorCode&);
+    };
 
-class NonEmptyEnvironment : public Environment {
-private:
-    friend class Environment;
+    class NonEmptyEnvironment : public Environment {
+    private:
+        friend class Environment;
 
-    bool has(const VariableName&) const override;
-    const Closure& lookup(const VariableName&) const override;
-    static NonEmptyEnvironment* create(const VariableName&, Closure&&, const Environment*, UErrorCode&);
-    virtual ~NonEmptyEnvironment();
-private:
-    friend class Environment;
+        bool has(const VariableName&) const override;
+        const Closure& lookup(const VariableName&) const override;
+        static NonEmptyEnvironment* create(const VariableName&, Closure&&, const Environment*, UErrorCode&);
+        virtual ~NonEmptyEnvironment();
+    private:
+        friend class Environment;
 
-    NonEmptyEnvironment(const VariableName& v, Closure&& c, Environment* e) : var(v), rhs(std::move(c)), parent(e) {}
+        NonEmptyEnvironment(const VariableName& v, Closure&& c, Environment* e) : var(v), rhs(std::move(c)), parent(e) {}
 
-    // Maps VariableName onto Closure*
-    // Chain of linked environments
-    VariableName var;
-    Closure rhs;
-    const LocalPointer<Environment> parent;
-};
+        // Maps VariableName onto Closure*
+        // Chain of linked environments
+        VariableName var;
+        Closure rhs;
+        const LocalPointer<Environment> parent;
+    };
 
-// Formatter cache
-// --------------
+    // Formatter cache
+    // --------------
 
-// Map from function names to Formatters
-class CachedFormatters : public UObject {
-private:
-    friend class MessageFormatter;
+    // Map from function names to Formatters
+    class CachedFormatters : public UObject {
+    private:
+        friend class MessageFormatter;
 
-    std::map<FunctionName, std::unique_ptr<Formatter>> cache;
-    CachedFormatters();
+        std::map<FunctionName, std::unique_ptr<Formatter>> cache;
+        CachedFormatters();
 
-public:
-    // Since Formatter is an interface, it's easiest to return a pointer here
-    const Formatter* getFormatter(const FunctionName&);
-    void setFormatter(const FunctionName&, Formatter*);
+    public:
+        // Since Formatter is an interface, it's easiest to return a pointer here
+        const Formatter* getFormatter(const FunctionName&);
+        void setFormatter(const FunctionName&, Formatter*);
 
-    CachedFormatters& operator=(const CachedFormatters&) = delete;
-    virtual ~CachedFormatters();
-};
+        CachedFormatters& operator=(const CachedFormatters&) = delete;
+        virtual ~CachedFormatters();
+    };
 
-// The context contains all the information needed to process
-// an entire message: arguments, formatter cache, and error list
+    // The context contains all the information needed to process
+    // an entire message: arguments, formatter cache, and error list
 
-class MessageFormatter;
+    class MessageFormatter;
 
-class MessageContext : public UMemory {
-public:
-    MessageContext(const MessageFormatter&, const MessageArguments&, const StaticErrors&);
+    class MessageContext : public UMemory {
+    public:
+        MessageContext(const MessageFormatter&, const MessageArguments&, const StaticErrors&);
 
-    bool isCustomFormatter(const FunctionName&) const;
-    const Formatter* maybeCachedFormatter(const FunctionName&, UErrorCode&);
-    const SelectorFactory* lookupSelectorFactory(const FunctionName&);
-    bool isSelector(const FunctionName& fn) const { return isBuiltInSelector(fn) || isCustomSelector(fn); }
-    bool isFormatter(const FunctionName& fn) const { return isBuiltInFormatter(fn) || isCustomFormatter(fn); }
+        bool isCustomFormatter(const FunctionName&) const;
+        const Formatter* maybeCachedFormatter(const FunctionName&, UErrorCode&);
+        const SelectorFactory* lookupSelectorFactory(const FunctionName&);
+        bool isSelector(const FunctionName& fn) const { return isBuiltInSelector(fn) || isCustomSelector(fn); }
+        bool isFormatter(const FunctionName& fn) const { return isBuiltInFormatter(fn) || isCustomFormatter(fn); }
 
-    bool hasGlobal(const VariableName& v) const { return hasGlobalAsFormattable(v) || hasGlobalAsObject(v); }
-    bool hasGlobalAsFormattable(const VariableName&) const;
-    bool hasGlobalAsObject(const VariableName&) const;
-    const Formattable& getGlobalAsFormattable(const VariableName&) const;
-    const UObject* getGlobalAsObject(const VariableName&) const;
+        bool hasGlobal(const VariableName& v) const { return hasGlobalAsFormattable(v) || hasGlobalAsObject(v); }
+        bool hasGlobalAsFormattable(const VariableName&) const;
+        bool hasGlobalAsObject(const VariableName&) const;
+        const Formattable& getGlobalAsFormattable(const VariableName&) const;
+        const UObject* getGlobalAsObject(const VariableName&) const;
 
-    // If any errors were set, update `status` accordingly
-    void checkErrors(UErrorCode& status) const;
-    DynamicErrors& getErrors() { return errors; }
+        // If any errors were set, update `status` accordingly
+        void checkErrors(UErrorCode& status) const;
+        DynamicErrors& getErrors() { return errors; }
 
-    const MessageFormatter& messageFormatter() const { return parent; }
+        const MessageFormatter& messageFormatter() const { return parent; }
 
-    virtual ~MessageContext();
+        virtual ~MessageContext();
 
-private:
+    private:
 
-    FormatterFactory* lookupFormatterFactory(const FunctionName&);
-    bool isBuiltInSelector(const FunctionName&) const;
-    bool isBuiltInFormatter(const FunctionName&) const;
-    bool isCustomSelector(const FunctionName&) const;
+        FormatterFactory* lookupFormatterFactory(const FunctionName&);
+        bool isBuiltInSelector(const FunctionName&) const;
+        bool isBuiltInFormatter(const FunctionName&) const;
+        bool isCustomSelector(const FunctionName&) const;
 
-    const MessageFormatter& parent;
-    const MessageArguments& arguments; // External message arguments
-    // Errors accumulated during parsing/formatting
-    DynamicErrors errors;
-}; // class MessageContext
+        const MessageFormatter& parent;
+        const MessageArguments& arguments; // External message arguments
+        // Errors accumulated during parsing/formatting
+        DynamicErrors errors;
+    }; // class MessageContext
 
 } // namespace message2
 
