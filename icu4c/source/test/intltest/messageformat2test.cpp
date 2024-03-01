ce51031ba5b0a023909cbcedf0d0f44b7391551f
Tim Chevalier <tjc@igalia.com>
2024-02-29 18:44:50 -0800

    Fixed handling of simple messages that begin with a '{'; tests in messageformat2test_icu pass now

--- icu4c/source/test/intltest/messageformat2test.cpp
+++ icu4c/source/test/intltest/messageformat2test.cpp
@@ -179,9 +179,9 @@ TestMessageFormat2::runIndexedTest(int32_t index, UBool exec,
                                   const char* &name, char* /*par*/) {
     TESTCASE_AUTO_BEGIN;
     /*
-    TESTCASE_AUTO(messageFormat1Tests);
     TESTCASE_AUTO(testAPICustomFunctions);
     */
+    TESTCASE_AUTO(messageFormat1Tests);
     TESTCASE_AUTO(featureTests);
     TESTCASE_AUTO(testCustomFunctions);
     TESTCASE_AUTO(testBuiltInFunctions);
