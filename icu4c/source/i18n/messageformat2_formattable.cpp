8cae40c9b92fa05193b8df034c1fa0192b7fad79
Tim Chevalier <tjc@igalia.com>
2024-09-30 18:19:49 -0700

    Fix warnings

--- icu4c/source/i18n/messageformat2_formattable.cpp
+++ icu4c/source/i18n/messageformat2_formattable.cpp
@@ -234,7 +234,7 @@ namespace message2 {
           type(kEvaluated) {
         CHECK_ERROR(status);
 
-        LocalPointer temp(create<FunctionOptions>(std::move(opts), status));
+        LocalPointer<FunctionOptions> temp(create<FunctionOptions>(std::move(opts), status));
         CHECK_ERROR(status);
         previousOptions = temp.orphan();
     }
@@ -261,7 +261,7 @@ namespace message2 {
     }
 
     void FormattedPlaceholder::initOptions(UErrorCode& status) {
-        LocalPointer temp(create<FunctionOptions>(FunctionOptions(), status));
+        LocalPointer<FunctionOptions> temp(create<FunctionOptions>(FunctionOptions(), status));
         CHECK_ERROR(status);
         previousOptions = temp.orphan();
     }
