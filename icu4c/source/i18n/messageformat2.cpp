acafbc6cea610025044c1b656eadda82a8573205
Tim Chevalier <tjc@igalia.com>
2024-10-04 16:38:05 -0700

    Rename functions

--- icu4c/source/i18n/messageformat2.cpp
+++ icu4c/source/i18n/messageformat2.cpp
@@ -26,11 +26,6 @@ using namespace data_model;
 // ------------------------------------------------------
 // Formatting
 
-// The result of formatting a literal is just itself.
-static Formattable evalLiteral(const Literal& lit) {
-    return Formattable(lit.unquoted());
-}
-
 // Assumes that `var` is a message argument; returns the argument's value.
 [[nodiscard]] InternalValue MessageFormatter::evalArgument(const VariableName& var,
                                                            MessageContext& context,
@@ -39,9 +34,12 @@ static Formattable evalLiteral(const Literal& lit) {
         // The fallback for a variable name is itself.
         UnicodeString str(DOLLAR);
         str += var;
+        // Look up the variable in the global environment
         const Formattable* val = context.getGlobal(var, errorCode);
         if (U_SUCCESS(errorCode)) {
+            // If it exists, create a BaseValue (FunctionValue) for it
             LocalPointer<BaseValue> result(BaseValue::create(locale, *val, errorCode));
+            // Add fallback and return an InternalValue
             if (U_SUCCESS(errorCode)) {
                 return InternalValue(result.orphan(), str);
             }
@@ -51,20 +49,23 @@ static Formattable evalLiteral(const Literal& lit) {
 }
 
 // Returns the contents of the literal
-[[nodiscard]] InternalValue MessageFormatter::formatLiteral(const Literal& lit,
-                                                            UErrorCode& errorCode) const {
-    // The fallback for a literal is itself.
-    LocalPointer<BaseValue> val(BaseValue::create(locale, evalLiteral(lit), errorCode));
+[[nodiscard]] InternalValue MessageFormatter::evalLiteral(const Literal& lit,
+                                                          UErrorCode& errorCode) const {
+    // Create a BaseValue (FunctionValue) that wraps the literal
+    LocalPointer<BaseValue> val(BaseValue::create(locale,
+                                                  Formattable(lit.unquoted()),
+                                                  errorCode));
     if (U_SUCCESS(errorCode)) {
+        // The fallback for a literal is itself.
         return InternalValue(val.orphan(), lit.quoted());
     }
     return {};
 }
 
-[[nodiscard]] InternalValue MessageFormatter::formatOperand(const Environment& env,
-                                                            const Operand& rand,
-                                                            MessageContext& context,
-                                                            UErrorCode &status) const {
+[[nodiscard]] InternalValue MessageFormatter::evalOperand(const Environment& env,
+                                                          const Operand& rand,
+                                                          MessageContext& context,
+                                                          UErrorCode &status) const {
     if (U_FAILURE(status)) {
         return {};
     }
@@ -76,17 +77,16 @@ static Formattable evalLiteral(const Literal& lit) {
         // Check if it's local or global
         // Note: there is no name shadowing; this is enforced by the parser
         const VariableName& var = rand.asVariable();
-        // TODO: Currently, this code implements lazy evaluation of locals.
+        // Currently, this code implements lazy evaluation of locals.
         // That is, the environment binds names to a closure, not a resolved value.
-        // Eager vs. lazy evaluation is an open issue:
-        // see https://github.com/unicode-org/message-format-wg/issues/299
+        // The spec does not require either eager or lazy evaluation.
 
         // Look up the variable in the environment
         if (env.has(var)) {
           // `var` is a local -- look it up
           const Closure& rhs = env.lookup(var);
-          // Format the expression using the environment from the closure
-          return formatExpression(rhs.getEnv(), rhs.getExpr(), context, status);
+          // Evaluate the expression using the environment from the closure
+          return evalExpression(rhs.getEnv(), rhs.getExpr(), context, status);
         }
         // Variable wasn't found in locals -- check if it's global
         InternalValue result = evalArgument(var, context, status);
@@ -103,7 +103,7 @@ static Formattable evalLiteral(const Literal& lit) {
         return result;
     } else {
         U_ASSERT(rand.isLiteral());
-        return formatLiteral(rand.asLiteral(), status);
+        return evalLiteral(rand.asLiteral(), status);
     }
 }
 
@@ -125,14 +125,11 @@ FunctionOptions MessageFormatter::resolveOptions(const Environment& env,
         const UnicodeString& k = opt.getName();
         const Operand& v = opt.getValue();
 
-        // Options are fully evaluated before calling the function
         // Format the operand
-        InternalValue rhsVal = formatOperand(env, v, context, status);
+        InternalValue rhsVal = evalOperand(env, v, context, status);
         if (U_FAILURE(status)) {
             return {};
         }
-        // Force evaluation in order to extract a FormattedPlaceholder
-        // from `rhsVal` (which might be a suspension)
         FunctionValue* optVal = rhsVal.takeValue(status);
 
         // The option is resolved; add it to the vector
@@ -191,7 +188,7 @@ FunctionOptions MessageFormatter::resolveOptions(const Environment& env,
 }
 
 // Formats an expression using `globalEnv` for the values of variables
-[[nodiscard]] InternalValue MessageFormatter::formatExpression(const Environment& globalEnv,
+[[nodiscard]] InternalValue MessageFormatter::evalExpression(const Environment& globalEnv,
                                                                const Expression& expr,
                                                                MessageContext& context,
                                                                UErrorCode &status) const {
@@ -200,8 +197,8 @@ FunctionOptions MessageFormatter::resolveOptions(const Environment& env,
     }
 
     const Operand& rand = expr.getOperand();
-    // Format the operand (formatOperand handles the case of a null operand)
-    InternalValue randVal = formatOperand(globalEnv, rand, context, status);
+    // Evaluate the operand (evalOperand handles the case of a null operand)
+    InternalValue randVal = evalOperand(globalEnv, rand, context, status);
 
     if (!expr.isFunctionCall()) {
         const FunctionValue* contained = randVal.getValue(status);
@@ -250,7 +247,10 @@ FunctionOptions MessageFormatter::resolveOptions(const Environment& env,
 }
 
 // Formats each text and expression part of a pattern, appending the results to `result`
-void MessageFormatter::formatPattern(MessageContext& context, const Environment& globalEnv, const Pattern& pat, UErrorCode &status, UnicodeString& result) const {
+void MessageFormatter::formatPattern(MessageContext& context,
+                                     const Environment& globalEnv,
+                                     const Pattern& pat,
+                                     UErrorCode &status, UnicodeString& result) const {
     CHECK_ERROR(status);
 
     for (int32_t i = 0; i < pat.numParts(); i++) {
@@ -261,7 +261,7 @@ void MessageFormatter::formatPattern(MessageContext& context, const Environment&
             // Markup is ignored
         } else {
 	      // Format the expression
-	      InternalValue partVal = formatExpression(globalEnv, part.contents(), context, status);
+	      InternalValue partVal = evalExpression(globalEnv, part.contents(), context, status);
               if (partVal.isFallback()) {
                   result += LEFT_CURLY_BRACE;
                   result += partVal.asFallback();
@@ -301,7 +301,7 @@ void MessageFormatter::resolveSelectors(MessageContext& context, const Environme
     // 2. For each expression exp of the message's selectors
     for (int32_t i = 0; i < dataModel.numSelectors(); i++) {
         // 2i. Let rv be the resolved value of exp.
-        InternalValue rv = formatExpression(env, selectors[i], context, status);
+        InternalValue rv = evalExpression(env, selectors[i], context, status);
         if (rv.isSelectable()) {
             // 2ii. If selection is supported for rv:
             // (True if this code has been reached)
@@ -361,7 +361,7 @@ void MessageFormatter::matchSelectorKeys(const UVector& keys,
 
     // Call the selector
     // Already checked for fallback, so it's safe to call takeValue()
-    LocalPointer rvVal(rv.takeValue(status));
+    LocalPointer<FunctionValue> rvVal(rv.takeValue(status));
     rvVal->selectKeys(adoptedKeys.getAlias(), keysLen, adoptedPrefs.getAlias(), prefsLen,
                       status);
 
