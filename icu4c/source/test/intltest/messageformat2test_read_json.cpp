81d9acf45ededa619521b9d186dded9fa92acc4a
Tim Chevalier <tjc@igalia.com>
2024-10-15 14:30:59 -0700

    Add u-options tests

--- icu4c/source/test/intltest/messageformat2test_read_json.cpp
+++ icu4c/source/test/intltest/messageformat2test_read_json.cpp
@@ -309,6 +309,9 @@ void TestMessageFormat2::jsonTestsFromFiles(IcuTestErrorCode& errorCode) {
     runTestsFromJsonFile(*this, "spec/functions/time.json", errorCode);
 
     // Other tests (non-spec)
+    // TODO: move this into the spec tests when
+    // https://github.com/unicode-org/message-format-wg/pull/846 lands
+    runTestsFromJsonFile(*this, "u-options.json", errorCode);
     runTestsFromJsonFile(*this, "more-functions.json", errorCode);
     runTestsFromJsonFile(*this, "valid-tests.json", errorCode);
     runTestsFromJsonFile(*this, "resolution-errors.json", errorCode);
